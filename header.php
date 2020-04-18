3ed45e61fe99bff79916d9a846f6168150a55aad
davy-web <chendavyweb@gmail.com>
2020-04-18 21:38:34 +0200

    Page accueil + style + bootstrap

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title><?php echo $title_page; ?></title>
+        <meta name="description" content="Produits frais bio, commander maintenant !">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/css2?family=Grand+Hotel&display=swap" rel="stylesheet">
+        <link rel="shortcut icon" type="image/x-icon" href="images/icon.png">
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+
+    <body>
+        <header>
+            
+        </header>
+        
---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+$title_page = "Accueil - Produits Bio";
+include 'header.php';
+?>
+
+        <!-- Meilleures Ventes -->
+        <div class="container">
+            <h1 class="font_hotel text-center">Meilleures Ventes</h1>
+            <p class="text-center">Et quadam quadam per incursare sollicite repperisset finitimis bellis sollicite ordinatus ferocissimas e usquam repperisset incessunt per explorabat moventem finitimis.</p>
+        </div>
+
+        <!-- 3 Produits -->
+
+        <!-- Fruits Bio -->
+        <div class="container-fluid fond_noir_1 pt-5">
+            <div class="container card-body">
+                <div class="row py-5">
+                    <div class="col-sm pt-5">
+                        <img src="images/fruits.jpg" alt="fruits bio" class="img-fluid rounded-circle">
+                    </div>
+                    <div class="col-sm color_white align-self-center pt-5">
+                        <h1 class="font_hotel color_green">Fruits Bio</h1>
+                        <p>Quid excidat exigue excidat amicitiam ac voluntatibus aut defluat ad sit quidem amicitia quae Hoc.</p>
+                        <button type="button" class="btn bouton_green">COMMANDER</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Ce qu'en disent nos clients -->
+        <div class="container-fluid fond_noir py-5">
+            <div class="container">
+                <h1 class="font_hotel text-center color_green">Ce qu'en disent nos clients :</h1>
+                <p class="text-center color_white">Et quadam quadam per incursare sollicite repperisset finitimis bellis sollicite ordinatus ferocissimas e usquam repperisset incessunt per explorabat moventem finitimis.</p>
+            </div>
+        </div>
+
+        <!-- Légumes Bio -->
+        <div class="container-fluid fond_noir_2 pb-5">
+            <div class="container card-body">
+                <div class="row py-5">
+                    <div class="col-sm color_white align-self-center pb-5">
+                        <h1 class="font_hotel color_green">Légumes Bio</h1>
+                        <p>Quid excidat exigue excidat amicitiam ac voluntatibus aut defluat ad sit quidem amicitia quae Hoc.</p>
+                        <button type="button" class="btn bouton_green">COMMANDER</button>
+                    </div>
+                    <div class="col-sm align-self-end pb-5">
+                        <img src="images/legumes.jpg" alt="légumes bio" class="img-fluid rounded-circle">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+<?php
+include 'footer.php';
+?>
