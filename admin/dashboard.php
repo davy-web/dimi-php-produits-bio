9be70f0a82e20b6827a5ea4e3d3ceb5044176365
davy-web <chendavyweb@gmail.com>
2020-05-10 06:28:04 +0200

    url

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -20,32 +20,33 @@ $nb_visit = $result5->fetch(PDO::FETCH_ASSOC);
 
                     <div class="container my-5">
                         <div class="row">
-                            <div class="col bg_grey mr-3 py-5 rounded text-center">
+                            <div class="col-lg-5 bg_grey py-5 rounded text-center">
                                 <p>Nombre de produits</p>
                                 <p class="size_l"><?php echo $nb_total_produits; ?></p>
                             </div>
-                            <div class="col bg_grey ml-3 py-5 rounded text-center">
+                            <div class="col-lg-2"></div>
+                            <div class="col-lg-5 bg_grey py-5 rounded text-center">
                                 <p>Nombre de messages</p>
                                 <p class="size_l"><?php echo $nb_total_messages; ?></p>
                             </div>
                         </div>
                         <div class="row rounded bg_grey mt-5 py-5 text-center">
-                            <div class="col-12 pb-5">
+                            <div class="col-lg-12 pb-5">
                                 <p class="font_hotel size_l color_green">Nombre de visite</p>
                             </div>
-                            <div class="col text-center">
+                            <div class="col-lg text-center">
                                 <p>Page Accueil</p>
                                 <p class="size_l"><?php echo $nb_visit['page_accueil']; ?></p>
                             </div>
-                            <div class="col text-center">
+                            <div class="col-lg text-center">
                                 <p>Page Produits</p>
                                 <p class="size_l"><?php echo $nb_visit['page_produits']; ?></p>
                             </div>
-                            <div class="col text-center">
+                            <div class="col-lg text-center">
                                 <p>Page Contact</p>
                                 <p class="size_l"><?php echo $nb_visit['page_contact']; ?></p>
                             </div>
-                            <div class="col text-center">
+                            <div class="col-lg text-center">
                                 <p>Page Nos valeurs</p>
                                 <p class="size_l"><?php echo $nb_visit['page_valeurs']; ?></p>
                             </div>
